a9aaf424b76538297b7b6fa97c31782f576ac57c
Joe <coming_soon>
2017-06-28 18:53:43 +0200

    PizzaMVC fixed

--- PizzaMVC/src/main/java/hu/soter/pizzamvc/HelloWorldController.java
+++ PizzaMVC/src/main/java/hu/soter/pizzamvc/HelloWorldController.java
@@ -1,13 +1,17 @@
 package hu.soter.pizzamvc;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloWorldController {
 
 	@RequestMapping("/wellhello")
-	public String sayHello() {
+	public String sayHello(@RequestParam("name") String name, Model model) {
+		model.addAttribute("jspname", name);
+		
 		return "hello-world";
 	}
 }
--- PizzaMVC/src/main/webapp/WEB-INF/jsp/hello-world.jsp
+++ PizzaMVC/src/main/webapp/WEB-INF/jsp/hello-world.jsp
@@ -7,6 +7,6 @@
 <title>Insert title here</title>
 </head>
 <body>
-	WellHello world.
+	WellHello ${jspname}.
 </body>
 </html>
